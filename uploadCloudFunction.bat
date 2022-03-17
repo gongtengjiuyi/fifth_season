e84bb88e384755c5e5c28ffee93c5b7ff4ba9269
gongtengjiuyi <gongtengjiuyi@qq.com>
2022-03-17 13:18:21 +0800

    create project

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-4gzgnlgfe0182ee3 --n quickstartFunctions --r --project "D:\WeChatProjects\miniprogram-2" --report_first --report
