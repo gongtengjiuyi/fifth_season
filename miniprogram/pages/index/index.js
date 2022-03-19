97929efd1716ac9ff96e7e99baaac642ae9c8881
gongtengjiuyi <gongtengjiuyi@qq.com>
2022-03-19 13:05:37 +0800

    add vant ui

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,84 +1,69 @@
 // pages/index/index.js
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    BannerList: [],
+    ChannelList: [],
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        BannerList: [],
-        ChannelList:[]
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: "https://m.taoyuewenhua.com/ajax/book_mall", //仅为示例，并非真实的接口地址
+      data: {
+        ctype: "2",
+        seed: "3716",
+      },
+      success: (res) => {
+        this.setData({
+          BannerList: [...res.data.data.bannerList],
+          ChannelList: [...res.data.data.channelList],
+        });
+        console.log(res.data);
+      },
+      fail: (err) => {
+        console.log(err);
+      },
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        var that=this
-        wx.request({
-            url: 'https://m.taoyuewenhua.com/ajax/book_mall', //仅为示例，并非真实的接口地址
-            data: {
-                ctype: '2',
-                seed: '3716'
-            },
-            success: (res) => {
-               this.setData({
-                    BannerList:[...res.data.data.bannerList],
-                    ChannelList:[...res.data.data.channelList]
-                })
-                console.log(res.data)
-            },
-            fail:err=>{
-                console.log(err)
-            }
-        })
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {},
+});
